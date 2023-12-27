d173dfb7a0faa7060a8690ff9be729cb06257ad4
Xavier Smith <117380947+StoicAutumn@users.noreply.github.com>
2023-12-27 11:39:04 -0500

    Fixedplayer choice pulling into playRound

--- scripts.js
+++ scripts.js
@@ -15,51 +15,55 @@ function game() {
 }
 console.log (game())
 */
+
 const buttons = document.querySelectorAll(".choiceButtons");
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    let buttontext = button.textContent;
+    let buttonText = button.textContent;
     let playerChoice = document.querySelector('#player-choice');
-    playerChoice.append(buttontext);
+    playerChoice.append(buttonText);
+    playRound()
+
+
+    // Plays one round of Rock, Paper, Scissors
+    function playRound() {
+      const playerSelection = buttonText;
+      const computerSelection = getComputerChoice();
+      let result = "";
+
+      if (playerSelection.toLowerCase() === computerSelection) {
+          result = "it's a tie! You chose " + playerSelection + " and your opponent chose " + computerSelection //+
+                //"\nPlayer Score: " + playerScore +
+                //"\nOpponent Score: " + computerScore
+      } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+                playerSelection === "scissors" && computerSelection === "paper" || 
+                playerSelection === "paper" && computerSelection === "rock") {
+          //playerScore++
+      result = "You win! " + playerSelection + " beats " + computerSelection //+
+             //"\nPlayer Score: " + playerScore +
+             //"\nOpponent Score: " + computerScore;
+      } else {
+          //computerScore++
+          result = "You lose! " + computerSelection + " beats " + playerSelection //+
+                //"\nPlayer Score: " + playerScore +
+                //"\nOpponent Score: " + computerScore;
+      } 
+      console.log ("Player: " + playerSelection);
+      console.log ("Opponent: " + computerSelection);
+      console.log (result);
+    }
+
+    // Generate computer's choice
+    function getComputerChoice() {
+      const randomNumber = Math.random();
+      if (randomNumber * 100 <= 33) {
+          return "rock";
+      } else if (randomNumber * 100 <= 66) {
+          return "paper";
+      } else {
+          return "scissors";
+      }
+    }
+
   });
 });
-
-// Plays one round of Rock, Paper, Scissors
-function playRound() {
-  //const playerSelection = prompt ("Rock, Paper, or Scissors?");
-  const computerSelection = getComputerChoice();
-  let result = "";
-
-  if (playerSelection.toLowerCase() === computerSelection) {
-      result = "it's a tie! You chose " + playerSelection + " and your opponent chose " + computerSelection +
-             "\nPlayer Score: " + playerScore +
-             "\nOpponent Score: " + computerScore
-  } else if (playerSelection === "rock" && computerSelection === "scissors" ||
-             playerSelection === "scissors" && computerSelection === "paper" || 
-             playerSelection === "paper" && computerSelection === "rock") {
-      playerScore++
-      result = "You win! " + playerSelection + " beats " + computerSelection +
-             "\nPlayer Score: " + playerScore +
-             "\nOpponent Score: " + computerScore;
-  } else {
-      computerScore++
-      result = "You lose! " + computerSelection + " beats " + playerSelection +
-             "\nPlayer Score: " + playerScore +
-             "\nOpponent Score: " + computerScore;
-  } 
-  console.log ("Player: " + playerSelection);
-  console.log ("Opponent: " + computerSelection);
-  console.log (result);
-}
-
-// Generate computer's choice
-function getComputerChoice() {
-  const randomNumber = Math.random();
-  if (randomNumber * 100 <= 33) {
-      return "rock";
-  } else if (randomNumber * 100 <= 66) {
-      return "paper";
-  } else {
-      return "scissors";
-  }
-}
